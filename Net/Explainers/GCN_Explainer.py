fc7568e5c04d09954ca69ebf8ac3f7b4d6c9343e
Ziyu-S <142449352+Ziyu-S@users.noreply.github.com>
2025-08-17 12:29:42 -0400

    Update GCN_Explainer.py

--- Net/Explainers/GCN_Explainer.py
+++ Net/Explainers/GCN_Explainer.py
@@ -147,12 +147,12 @@ if __name__ == "__main__":
     torch.backends.cudnn.benchmark = False
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         
-    model_dir = "/home/ziyu-song/pKa/Results/GCN_Grid_Search/6/saved_models/dataset_2/loss_MSELoss_h64_b16_p20_f10_lr0.006_d0.3"
+    model_dir = "/Graph_pKa/Results/GCN_Grid_Search/6/saved_models/dataset_2/loss_MSELoss_h64_b16_p20_f10_lr0.006_d0.3"
     hparams, pth_files = load_models_and_hparams(model_dir)
     print(f"Found {len(pth_files)} pth files")
     print("Detected hyperparameters:", {k: v for k, v in hparams.items() if k.endswith("_name") or k in ("hidden_channels","batch_size","learning_rate","dropout")})
 
-    data_pkl_path = "/home/ziyu-song/Graph_pKa/Data/4_Residues_W_Local_Frame/Subsets/data_list_2.pkl"
+    data_pkl_path = "/Graph_pKa/Data/4_Residues_W_Local_Frame/Subsets/data_list_2.pkl"
     
     procs = 1 if device.type == "cuda" else min(len(pth_files), mp.cpu_count())
     
@@ -175,6 +175,6 @@ if __name__ == "__main__":
         'Feature_Index': np.arange(len(global_feature_importance)),
         'Importance': global_feature_importance
     })
-    mean_importance_df.to_csv('/home/ziyu-song/pKa/Results/GCN_Grid_Search/6/Feature_Importance/GCN_Feature_Importance_1.csv', index=False)
+    mean_importance_df.to_csv('/Graph_pKa/Results/GCN_Grid_Search/6/Feature_Importance/GCN_Feature_Importance_1.csv', index=False)
 
 
